e5c0db80cdb351bfddd13122b8f6bf3039ee5be1
TTwaits <7724196586@qq.com>
2018-10-30 16:02:44 +0800

    robot 热门资讯、资讯追踪

--- src/main/java/com/nanfang/robot/controller/RobotController.java
+++ src/main/java/com/nanfang/robot/controller/RobotController.java
@@ -2,10 +2,12 @@ package com.nanfang.robot.controller;
 
 import com.nanfang.robot.util.APIClient;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,19 +18,22 @@ import java.util.Map;
 public class RobotController {
     private static final String SECRET_ID = "bff22dae2936490b813f287a6991ed30";
     private static final String SECRET_KEY = "7a8dfe0190014700bf07f63b444fc12d";
-    @RequestMapping("/hello")
-    public String helloHtml(HashMap<String,Object> map){
-        map.put("hello", "欢迎进入HTML页面");
-        return "/index";
+
+    @RequestMapping("/hotArticle")
+    public String hotArticle() {
+        return "hot_article";
     }
+
     /**
      * 获取热门资讯
      */
     @RequestMapping("/recommender/hot_article")
-    public String hotArticle(HttpServletRequest request){
+    @ResponseBody
+    public String hotArticle(HttpServletRequest request) {
         try {
+            String field = request.getParameter("field");
             Map<String, String> params = new HashMap<>(16);
-            params.put("field", "体育");
+            params.put("field", field);
             String result = APIClient.post("http://api.giiso.ai/openapi/recommender/hot_article", SECRET_ID,
                     SECRET_KEY, params);
             return result;
@@ -37,31 +42,39 @@ public class RobotController {
         }
         return "";
     }
+    @RequestMapping("/pageToKeyword")
+    public String pageToKeyword(){
+        return "keyword";
+    }
+
     /**
      * 添加关键词
      */
-    public String keywordAdd(){
+    @RequestMapping("/keyword/add")
+    public void keywordAdd(String keyword, String customId, HttpServletResponse response) {
         try {
             Map<String, String> params = new HashMap<>(16);
-            params.put("customId", "");
-            params.put("keyword", "");
+            params.put("customId", "15914455733");
+            params.put("keyword", keyword);
             String result = APIClient.post("http://api.giiso.ai/openapi/subscribe/keyword/add", SECRET_ID,
                     SECRET_KEY, params);
-            return result;
+            response.sendRedirect("/pageToKeyword");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "";
     }
+
     /**
      * 删除关键词
      */
-    public String keywordDel(){
+    @RequestMapping("/keyword/del")
+    @ResponseBody
+    public String keywordDel(String keyword,String customId) {
         try {
             Map<String, String> params = new HashMap<>(16);
 
-            params.put("customId", "");
-            params.put("keyword", "");
+            params.put("customId", customId);
+            params.put("keyword", keyword);
 
             String result = APIClient.post("http://api.giiso.ai/openapi/subscribe/keyword/del", SECRET_ID,
                     SECRET_KEY, params);
@@ -75,11 +88,13 @@ public class RobotController {
     /**
      * 获取关键词列表
      */
-    public String keywordList(){
+    @RequestMapping("/keyword/list")
+    @ResponseBody
+    public String keywordList(String customId) {
         try {
             Map<String, String> params = new HashMap<>(16);
 
-            params.put("customId", "");
+            params.put("customId", customId);
 
             String result = APIClient.post("http://api.giiso.ai/openapi/subscribe/keyword/list", SECRET_ID,
                     SECRET_KEY, params);
@@ -88,5 +103,30 @@ public class RobotController {
             e.printStackTrace();
         }
         return "";
+    }
+
+    @RequestMapping("/subscribe/news")
+    @ResponseBody
+    public String subscribeNews(String keyword,String customId) {
+        try {
+            Map<String, String> params = new HashMap<>(16);
+
+            params.put("customId", customId);
+            params.put("keyword", keyword);
+
+            String result = APIClient.post("http://api.giiso.ai/openapi/subscribe/news", SECRET_ID,
+                    SECRET_KEY, params);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    @RequestMapping("/pageToSubscribeNews")
+    public String pageToSubscribeNews(ModelMap map, String keyword, String customId) {
+        map.addAttribute("keyword",keyword);
+        map.addAttribute("customId",customId);
+        return "subscribe_news";
     }
 }
